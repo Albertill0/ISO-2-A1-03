d7a7eb23e896b52d881ec96cb17a48719be84a1f
AlejandroGalvezUCLM <92250656+AlejandroGalvezUCLM@users.noreply.github.com>
2021-12-02 19:08:04 +0100

    Added implementation iteration3

---
+++ PatientsManagementProject/Iteration3/src/main/java/es/uclm/esi/isoft2/a03/PatientsManagementProject/Iteration3/Domain/Campaign.java
@@ -0,0 +1,21 @@
+package es.uclm.esi.isoft2.a03.PatientsManagementProject.Iteration3.Domain;
+
+import es.uclm.esi.isoft2.a03.PatientsManagementProject.Iteration3.Persistence.CampaignDAO;
+
+public class Campaign {
+	private int _id;
+	private String _name;
+	private int _cost;
+	private String _dateInit;
+	private String _dateEnd;
+	private float _effectiveness;
+	public CampaignDAO _unnamed_CampaignDAO_;
+
+	public void Campaign(int aId, String aName, int aCost, String aDateInt, String aDateEnd, float aEffectiveness) {
+		throw new UnsupportedOperationException();
+	}
+
+	public int insertCampaign() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ PatientsManagementProject/Iteration3/src/main/java/es/uclm/esi/isoft2/a03/PatientsManagementProject/Iteration3/Presentation/JFrameCampaignSupervisor.java
@@ -0,0 +1,12 @@
+package es.uclm.esi.isoft2.a03.PatientsManagementProject.Iteration3.Presentation;
+
+public class JFrameCampaignSupervisor {
+
+	public String showResult() {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean askConfirmation(String aData) {
+		throw new UnsupportedOperationException();
+	}
+}
