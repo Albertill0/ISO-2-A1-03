bffe6c52184a27c972fe7e6c85d9fcd8c57dfe38
pavalso <pablo.valver@alu.uclm.es>
2021-12-14 19:16:12 +0100

    Added files

---
+++ PatientsManagementProject/Iteration12/src/main/java/es/uclm/esi/isoft2/a03/PatientsManagementProject/Iteration12/Domain/Campaign.java
@@ -0,0 +1,22 @@
+package es.uclm.esi.isoft2.a03.PatientsManagementProject.Iteration12.Domain;
+
+public class Campaign {
+	private int _id;
+	private String _name;
+	private String _dateInit;
+	private String _dateEnd;
+	private float _effectiveness;
+	public CampaignDAO _unnamed_CampaignDAO_;
+
+	public Campaign() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Campaign(int aId, String aName, int aCost, String aDateInt, String aDateEnd, float aEffectiveness) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void readNumberCampaigns() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ PatientsManagementProject/Iteration12/src/main/java/es/uclm/esi/isoft2/a03/PatientsManagementProject/Iteration12/Domain/Vaccine.java
@@ -0,0 +1,19 @@
+package es.uclm.esi.isoft2.a03.PatientsManagementProject.Iteration12.Domain;
+
+public class Vaccine {
+	private int _id;
+	private int _quantity;
+	public VaccineDAO _unnamed_VaccineDAO_;
+
+	public Vaccine() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Vaccine(int aId, int aQuality) {
+		throw new UnsupportedOperationException();
+	}
+
+	public int listVaccines() {
+		throw new UnsupportedOperationException();
+	}
+}
