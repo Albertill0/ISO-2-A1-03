63094a8d76a7a47dea4100fd2381bce4f63c0038
AlejandroGalvezUCLM <92250656+AlejandroGalvezUCLM@users.noreply.github.com>
2021-12-14 13:13:04 +0100

    Fixed errors v3.0,1

--- PatientsManagementProject/Iteration3/src/main/java/es/uclm/esi/isoft2/a03/PatientsManagementProject/Iteration3/Presentation/JFrameCampaignSupervisor.java
+++ PatientsManagementProject/Iteration3/src/main/java/es/uclm/esi/isoft2/a03/PatientsManagementProject/Iteration3/Presentation/JFrameCampaignSupervisor.java
@@ -1,12 +1,18 @@
 package es.uclm.esi.isoft2.a03.PatientsManagementProject.Iteration3.Presentation;
 
+import es.uclm.esi.isoft2.a03.PatientsManagementProject.Iteration3.Domain.Campaign;
+
 public class JFrameCampaignSupervisor {
 
 	public String showResult() {
 		throw new UnsupportedOperationException();
 	}
 
-	public boolean askConfirmation(String aData) {
+	public boolean askConfirmation() {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean addCampaign(Campaign aC) {
 		throw new UnsupportedOperationException();
 	}
 }
